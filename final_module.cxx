1856ddce7b1cf52fee3ff0523d41662b0436d516
Konstantin Olchanski <olchansk@daq00.triumf.ca>
2021-12-10 16:52:14 -0800

    add bsc64_bus histograms

--- final_module.cxx
+++ final_module.cxx
@@ -63,6 +63,9 @@ public:
    TH2D* h_bsc_bsc_adc_time;
    TH2D* h_bsc_bsc_adc_amp;
 
+   TH1D* h_bsc64_bits;
+   TH2D* h_bsc64_vs_bsc_adc_hits;
+
    TH1D* h_aw_num_hits;
 
    TH1D* h_aw_time;
@@ -125,8 +128,11 @@ public:
    TH1D* h_aw16_prompt_bits;
    TH2D* h_aw16_prompt_bits_vs_aw;
 
-   TH2D* h_aw_bsc_adc_hits;
-   TH2D* h_aw_pc_bsc_adc_hits;
+   TH2D* h_aw_vs_bsc_adc_hits;
+   TH2D* h_aw_pc_vs_bsc_adc_hits;
+
+   TH2D* h_aw16_prompt_vs_bsc64 = NULL;
+   TH2D* h_aw16_prompt_vs_bsc_adc_hits = NULL;
 
    TH1D* h_pad_num_hits;
 
@@ -146,7 +152,9 @@ public:
    TH2D* h_pad_time_per_column = NULL;
    TH2D* h_pad_time_per_row = NULL;
 
+#if 0
    TH1D* h_pad_time_pos[64];
+#endif
 
    TH2D* h_aw_pad_num_hits;
    TH2D* h_aw_pad_hits;
@@ -164,6 +172,8 @@ public:
    {
       if (fTrace)
          printf("FinalModule::ctor!\n");
+
+      fModuleName = "final_module";
 
       fPrint = do_print;
    }
@@ -207,6 +217,9 @@ public:
       h_bsc_bsc_adc_hits = new TH2D("h_bsc_bsc_adc_hits", "hits in bsc adc vs bsc adc", NUM_BSC, -0.5, NUM_BSC-0.5, NUM_BSC, -0.5, NUM_BSC-0.5);
       h_bsc_bsc_adc_time = new TH2D("h_bsc_bsc_adc_time", "time in bsc adc vs bsc adc", 50, 0, MAX_TIME, 50, 0, MAX_TIME);
       h_bsc_bsc_adc_amp  = new TH2D("h_bsc_bsc_adc_amp",  "p.h. in bsc adc vs bsc adc", 50, 0, MAX_AW_AMP, 50, 0, MAX_AW_AMP);
+
+      h_bsc64_bits = new TH1D("h_bsc64_bits", "FPGA bsc64 bits; bsc64 bit 0..63", 64+1, -0.5, 64-0.5+1);
+      h_bsc64_vs_bsc_adc_hits = new TH2D("h_bsc64_vs_bsc_adc_hits", "FPGA bsc64 bits vs BSC ADC hit bar number; bsc64 bit 0..63; bar number", 64, -0.5, 64-0.5, NUM_BSC, -0.5, NUM_BSC-0.5);
 
       h_aw_num_hits = new TH1D("h_aw_num_hits", "number of anode wire hits", 100, 0, MAX_HITS);
       h_aw_time = new TH1D("h_aw_time", "aw hit time; time, ns", 100, 0, MAX_TIME);
@@ -269,8 +282,12 @@ public:
       h_aw16_prompt_bits = new TH1D("h_aw16_prompt_bits", "FPGA aw16_prompt bits; link bit 0..15", 16+1, -0.5, 16-0.5+1);
       h_aw16_prompt_bits_vs_aw = new TH2D("h_aw16_prompt_bits_vs_aw", "FPGA aw16_prompt bits vs AW tpc wire number; tpc wire number; link bit 0..15", NUM_AW, -0.5, NUM_AW-0.5, 16, -0.5, 16-0.5);
 
-      h_aw_bsc_adc_hits = new TH2D("h_aw_bsc_adc_hits", "hits in aw vs bsc adc", NUM_AW, -0.5, NUM_AW-0.5, NUM_BSC, -0.5, NUM_BSC-0.5);
-      h_aw_pc_bsc_adc_hits = new TH2D("h_aw_pc_bsc_adc_hits", "hits in aw (PC region) vs bsc adc", NUM_AW, -0.5, NUM_AW-0.5, NUM_BSC, -0.5, NUM_BSC-0.5);
+      h_aw_vs_bsc_adc_hits = new TH2D("h_aw_vs_bsc_adc_hits", "hits in aw vs bsc adc; wire number; bar number", NUM_AW, -0.5, NUM_AW-0.5, NUM_BSC, -0.5, NUM_BSC-0.5);
+      h_aw_pc_vs_bsc_adc_hits = new TH2D("h_aw_pc_vs_bsc_adc_hits", "hits in aw (PC region) vs bsc adc; wire number; bar number", NUM_AW, -0.5, NUM_AW-0.5, NUM_BSC, -0.5, NUM_BSC-0.5);
+
+      h_aw16_prompt_vs_bsc64 = new TH2D("h_aw16_prompt_vs_bsc64", "FPGA aw16_prompt hits vs bsc64 hits; bsc64 bit; aw16_prompt bit", 64, -0.5, 64-0.5, 16, -0.5, 16-0.5);
+
+      h_aw16_prompt_vs_bsc_adc_hits = new TH2D("h_aw16_prompt_vs_bsc_adc_hits", "FPGA aw16_prompt hits vs bsc adc hits; aw16_prompt bit; bsc bar number", 16, -0.5, 16-0.5, NUM_BSC, -0.5, NUM_BSC-0.5);
 
       h_pad_num_hits = new TH1D("h_pad_num_hits", "number of pad hits; number of hits in pads", 100, 0, MAX_HITS);
       h_pad_time = new TH1D("h_pad_time", "pad hit time; time, ns", 100, 0, MAX_TIME);
@@ -285,6 +302,7 @@ public:
       h_pad_time_per_column = new TH2D("h_pad_time_per_column", "pad hit time per column; tpc column; time, ns", 32, -0.5, 32-0.5, 100, 0, MAX_TIME);
       h_pad_time_per_row = new TH2D("h_pad_time_per_row", "pad hit time per row; tpc row; time, ns", 8*4*18, -0.5, 8*4*18-0.5, 100, 0, MAX_TIME);
 
+#if 0
       for (int icol=0; icol<8; icol++) {
          for (int iring=0; iring<8; iring++) {
             char name[256];
@@ -294,6 +312,7 @@ public:
             h_pad_time_pos[icol*8+iring] = new TH1D(name, title, 100, 0, MAX_TIME);
          }
       }
+#endif
 
       //int npads = MAX_FEAM*MAX_FEAM_PAD_COL*MAX_FEAM_PAD_ROWS;
       //h_pad_pad_num_hits = new TH2D("h_pad_pad_num_hits", "pad number vs pad number; pad number, col*N+row; pad number, col*N+row", npads, -0.5, npads-0.5, npads, -0.5, npads-0.5);
@@ -357,7 +376,12 @@ public:
 
       //uint32_t adc16_coinc_dff = 0;
       uint32_t aw16_prompt = 0;
+      int aw16_mlu_out = 0;
       uint32_t trig_bitmap  = 0;
+      uint32_t aw16_bus = 0;
+      uint64_t bsc64_bus = 0;
+      int bsc64_mult = 0;
+      uint32_t coinc_latch = 0;
 
       //if (age->trig && age->trig->udpData.size() > 7) {
       //   adc16_coinc_dff = (age->trig->udpData[6]>>8)&0xFFFF;
@@ -365,11 +389,29 @@ public:
 
       if (age->trig && age->trig->udpData.size() > 9) {
          aw16_prompt = (age->trig->udpData[9])&0xFFFF;
+         aw16_mlu_out = ((age->trig->udpData[9])&0x80000000) != 0;
       }
 
       if (age->trig && age->trig->udpData.size() > 7) {
          trig_bitmap = age->trig->udpData[6];
       }
+
+      if (age->trig && age->trig->udpData.size() > 13) {
+         aw16_bus = age->trig->udpData[13] & 0xFFFF;
+      }
+
+      if (age->trig && age->trig->udpData.size() > 17) {
+         uint64_t bsc64_bus_lo = age->trig->udpData[14];
+         uint64_t bsc64_bus_hi = age->trig->udpData[15];
+         bsc64_bus = (bsc64_bus_hi << 32) | bsc64_bus_lo;
+         bsc64_mult = age->trig->udpData[16] & 0xFF;
+      }
+
+      if (age->trig && age->trig->udpData.size() > 17) {
+         coinc_latch = age->trig->udpData[17] & 0xFF;
+      }
+
+      //printf("trig_bitmap 0x%08x, aw16_prompt 0x%04x, aw16_mlu_out %d, aw16_bus 0x%04x, bsc64_bus 0x%016lx, bsc64_mult %2d, coinc_latch 0x%02x\n", trig_bitmap, aw16_prompt, aw16_mlu_out, aw16_bus, bsc64_bus, bsc64_mult, coinc_latch);
 
       int trig_counter = -1;
       int adc_counter = -1;
@@ -414,6 +456,26 @@ public:
          }
       }
 
+      if (bsc64_bus) {
+         for (uint64_t i=0; i<64; i++) {
+            if (bsc64_bus & (((uint64_t)1)<<i)) {
+               h_bsc64_bits->Fill(i);
+            }
+         }
+      }
+
+      if (aw16_prompt && bsc64_bus) {
+         for (int i=0; i<16; i++) {
+            if (aw16_prompt & (1<<i)) {
+               for (uint64_t j=0; j<64; j++) {
+                  if (bsc64_bus & (((uint64_t)1)<<j)) {
+                     h_aw16_prompt_vs_bsc64->Fill(j, i);
+                  }
+               }
+            }
+         }
+      }
+
       if (eba) {
          if (fPrint) {
             printf("BA event %d, time %f, bsc adc hits: %d\n", ef->fEvent->counter, ef->fEvent->time, (int)eba->fBscAdcHits.size());
@@ -444,6 +506,35 @@ public:
                h_bsc_bsc_adc_time->Fill(eba->fBscAdcHits[j].time, eba->fBscAdcHits[k].time);
                h_bsc_bsc_adc_amp->Fill(eba->fBscAdcHits[j].amp, eba->fBscAdcHits[k].amp);
             }
+
+            if (bsc64_bus) {
+               for (uint64_t i=0; i<64; i++) {
+                  if (bsc64_bus & (((uint64_t)1)<<i)) {
+                     h_bsc64_vs_bsc_adc_hits->Fill(i, bar);
+                  }
+               }
+            }
+
+            if (aw16_prompt) {
+               //printf("aw16_prompt: 0x%04x\n", aw16_prompt);
+               for (int i=0; i<16; i++) {
+                  if (aw16_prompt & (1<<i)) {
+                     h_aw16_prompt_vs_bsc_adc_hits->Fill(i, bar);
+                  }
+               }
+            }
+
+            //if (bar == 64+63) {
+            //   printf("bar %3d, aw16_prompt 0x%04x, bsc64_bus 0x%016lx bit 0x%016lx %d\n", bar, aw16_prompt, bsc64_bus, bsc64_bus&(((uint64_t)1)<<bar), (bsc64_bus&(((uint64_t)1)<<bar))!=0);
+            //}
+
+            //int xbit = 32;
+            //if (bsc64_bus&(((uint64_t)1)<<xbit)) {
+            //   if (amp < 8000)
+            //      continue;
+            //   printf("bsc64 bit %2d, bar %3d, time %6.0f, amp %6.0f, aw16_prompt 0x%04x, bsc64_bus 0x%016lx bit 0x%016lx %d\n", xbit, bar, time, amp, aw16_prompt, bsc64_bus, bsc64_bus&(((uint64_t)1)<<bar), (bsc64_bus&(((uint64_t)1)<<bar))!=0);
+            //
+            //}
          }
       }
 
@@ -451,9 +542,9 @@ public:
          for (unsigned j=0; j<eba->fBscAdcHits.size(); j++) {
             for (unsigned k=0; k<eawh->fAwHits.size(); k++) {
                double time = eawh->fAwHits[k].time;
-               h_aw_bsc_adc_hits->Fill(eawh->fAwHits[k].wire, eba->fBscAdcHits[j].bar);
+               h_aw_vs_bsc_adc_hits->Fill(eawh->fAwHits[k].wire, eba->fBscAdcHits[j].bar);
                if (time >= 800 && time < 1200) {
-                  h_aw_pc_bsc_adc_hits->Fill(eawh->fAwHits[k].wire, eba->fBscAdcHits[j].bar);
+                  h_aw_pc_vs_bsc_adc_hits->Fill(eawh->fAwHits[k].wire, eba->fBscAdcHits[j].bar);
                }
             }
          }
@@ -606,7 +697,9 @@ public:
             int pwb_seqpos = pwb_col*8 + pwb_ring;
             assert(pwb_seqpos >= 0 && pwb_seqpos < 64);
 
+#if 0
             h_pad_time_pos[pwb_seqpos]->Fill(time);
+#endif
 
 #if 0
             for (unsigned ii=0; ii<eph->fPadHits.size(); ii++) {
