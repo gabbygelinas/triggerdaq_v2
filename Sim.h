f103e9ace8134d58aeea1623d5e4a56f769f670f
Gareth William Smith <gareth.william.smith@cern.ch>
2023-07-04 16:09:09 -0700

    remove containers for possible future data

--- Sim.h
+++ Sim.h
@@ -25,8 +25,6 @@ class SimEvent
    std::string fError_message; // error message
    int  fCounter = 0;      // event sequential counter
    std::array<double, 3> fVertex;  // simulated verex position
-   std::map<std::string, double> fSimDoubles; // other data from simulation, left open for the future
-   std::map<std::string, double> fSimInts; // other data from simulation, left open for the future
 
  public:
    SimEvent(); // ctor
@@ -36,16 +34,6 @@ class SimEvent
    void Print(int level=0) const;
 
 public:
-    void SetDouble(const std::string& key, const double value) {fSimDoubles[key] = value;}
-    double GetDouble(const std::string& key) {
-      if (fSimDoubles.count(key)>0) return fSimDoubles[key];
-      else return 0;
-    }
-    void SetInt(const std::string& key, const int value) {fSimInts[key] = value;}
-    double GetInt(const std::string& key) {
-      if (fSimInts.count(key)>0) return fSimInts[key];
-      else return 0;
-    }
     void SetVertex(std::array<double, 3> a_vertex) { fVertex = a_vertex; }
     void SetVertexX(double x) { fVertex[0] = x;}
     void SetVertexY(double y) { fVertex[1] = y;}
