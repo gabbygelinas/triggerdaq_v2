00caaacd198989161cfbba5641c07ff131720706
DarkLight DAQ <dldaq@daq13.triumf.ca>
2024-06-12 12:48:12 -0700

    KO - cleanup DlFlow.h

--- DlFlow.h
+++ DlFlow.h
@@ -1,115 +1,15 @@
 //
-// AgFlow.h
+// DlFlow.h
 //
-// manalyzer flow objects for ALPHA-g events
+// manalyzer flow objects for DARK-LIGHT events
 // K.Olchanski
 //
 
-#ifndef AgFlow_H
-#define AgFlow_H
+#ifndef DlFlow_H
+#define DlFlow_H
 
-//#include "AgEvent.h"
 #include "unpack_cb.h"
 #include "manalyzer.h"
-
-#if 0
-class AgEventFlow: public TAFlowEvent
-{
- public:
-   AgEvent *fEvent;
-   
- public:
- AgEventFlow(TAFlowEvent* flow, AgEvent* e) // ctor
-    : TAFlowEvent(flow)
-   {
-      fEvent = e;
-   }
-   
-   ~AgEventFlow() // dtor
-      {
-         if (fEvent) {
-            delete fEvent;
-            fEvent = NULL;
-         }
-      }
-};
-
-struct AgAwHit
-{
-   int adc_module; // ADC module, 1..20
-   int adc_chan; // ADC channel, 0..15 and 16..47.
-   int wire; // anode wire, 0..255 bottom, 256..511 top
-   double time; // hit time, ns
-   double amp;  // hit amplitude
-};
-
-class AgAwHitsFlow: public TAFlowEvent
-{
- public:
-   std::vector<AgAwHit> fAwHits;
-   
- public:
- AgAwHitsFlow(TAFlowEvent* flow) // ctor
-    : TAFlowEvent(flow)
-   {
-   }
-};
-
-struct AgBscAdcHit
-{
-   int adc_module; // ADC module, 1..20
-   int adc_chan; // ADC channel, 0..15 and 16..47.
-   int bar; // bar number, 0..63 bottom, 64..127 top
-   double time; // hit time, ns
-   double amp;  // hit amplitude
-};
-
-class AgBscAdcHitsFlow: public TAFlowEvent
-{
- public:
-   std::vector<AgBscAdcHit> fBscAdcHits;
-   
- public:
- AgBscAdcHitsFlow(TAFlowEvent* flow) // ctor
-    : TAFlowEvent(flow)
-   {
-   }
-};
-
-struct AgPadHit
-{
-   int imodule; // pwbNN
-   int seqsca; // sca*80+ri
-   int tpc_col; // pad column
-   int tpc_row; // pad row
-   double time_ns; // hit time in ns
-   double amp;  // hit amplitude
-};
-
-class AgPadHitsFlow: public TAFlowEvent
-{
- public:
-   std::vector<AgPadHit> fPadHits;
-   
- public:
- AgPadHitsFlow(TAFlowEvent* flow) // ctor
-    : TAFlowEvent(flow)
-   {
-   }
-};
-
-class AgTrigUdpFlow: public TAFlowEvent
-{
- public:
-   std::vector<uint32_t> fData;
-   
- public:
- AgTrigUdpFlow(TAFlowEvent* flow) // ctor
-    : TAFlowEvent(flow)
-   {
-   }
-};
-#endif
 
 class CbHitsFlow: public TAFlowEvent
 {
